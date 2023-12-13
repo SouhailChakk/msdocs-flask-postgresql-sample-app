319c97578ae9195dc66a393ae3c4d8ec50368006
SouhailChakk <134346382+SouhailChakk@users.noreply.github.com>
2023-12-13 15:17:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 import os
 from azure.storage.blob import BlobServiceClient
+from werkzeug.utils import secure_filename
+
 
 
 
