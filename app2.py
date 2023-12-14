42f958c78f3d7b96b5329bd308473eda14c9ce91
SouhailChakk <134346382+SouhailChakk@users.noreply.github.com>
2023-12-14 17:18:17 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,59 +1,57 @@
-import os
-from flask import Flask, request, redirect, url_for, Blueprint
-from flask_wtf.csrf import CSRFProtect
-from werkzeug.utils import secure_filename
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+# app2.py
 
+import os
+from flask import Blueprint, request, redirect, url_for, render_template
+from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
+from azure.storage.blob import BlobServiceClient
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
 
 app2_bp = Blueprint('app2', __name__)
-csrf = CSRFProtect(app)
+csrf = CSRFProtect()
+
+# Connect to Azure Storage Blob
+connect_str = os.getenv('DefaultEndpointsProtocol=https;AccountName=csb10032000d733470a;AccountKey=7xF+CGBtqhNfWAoBCPQrp3cyp+qsDH+moJ9Np00KFNpkslSMNbuYW+/VzdHdxGdoZwMrDNgU5sKq+AStdVUzBQ==;EndpointSuffix=core.windows.net') 
+container_name = "blobstorage"
+blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str)
 
 
-def create_app():
-    app = Flask(__name__)
-    csrf = CSRFProtect(app)
 
-    # Configure Azure Storage
-    connect_str = os.getenv('DefaultEndpointsProtocol=https;AccountName=csb10032000d733470a;AccountKey=7xF+CGBtqhNfWAoBCPQrp3cyp+qsDH+moJ9Np00KFNpkslSMNbuYW+/VzdHdxGdoZwMrDNgU5sKq+AStdVUzBQ==;EndpointSuffix=core.windows.net')
-    container_name = "blobstorage"
-    blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str)
+try:
+    container_client = blob_service_client.get_container_client(container=container_name)
+    container_client.get_container_properties()
+except Exception as e:
+    print(e)
+    print("Creating container...")
+    container_client = blob_service_client.create_container(container_name)
 
-    try:
-        container_client = blob_service_client.get_container_client(container=container_name)
-        container_client.get_container_properties()
-    except Exception as e:
-        print(e)
-        print("Creating container...")
-        container_client = blob_service_client.create_container(container_name)
-
-    return app
-
-app = create_app()
-
-@app2_bp.route('/upload', methods=['POST'])
 @csrf.exempt
-def upload_image():
-    if 'file' not in request.files:
-        return redirect(request.url)
+@app2_bp.route('/upload_image/<int:id>', methods=['POST'])
+def upload_image(id):
+    # Ensure the 'customer_image' file is part of the form
+    if 'customer_image' not in request.files:
+        # Handle the case where no file is selected
+        return redirect(url_for('app.details', id=id))
 
-    file = request.files['file']
+    file = request.files['customer_image']
 
+    # Handle the case where the user submits an empty form
     if file.filename == '':
-        return redirect(request.url)
+        return redirect(url_for('app.details', id=id))
 
-    if file:
-        filename = secure_filename(file.filename)
+    # Save the file to blob storage
+    save_image_to_blob(file, id)
 
-        # Upload the file to Azure Blob Storage
-        blob_client = container_client.get_blob_client(filename)
-        blob_client.upload_blob(file.read(), overwrite=True)
+    return redirect(url_for('app.details', id=id))
 
-        # Add additional logic, such as saving the blob URL to the database
+def save_image_to_blob(file, restaurant_id):
+    # Save the file to blob storage using the appropriate method
+    # (you need to adapt this part based on your blob storage setup)
+    filename = secure_filename(file.filename)  # Make sure to import secure_filename
+    blob_client = container_client.get_blob_client(filename)
 
-        return 'File uploaded successfully'
-
-        return 'File uploaded successfully'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Upload the file to blob storage
+    with file.stream as data:
+        blob_client.upload_blob(data, overwrite=True)
