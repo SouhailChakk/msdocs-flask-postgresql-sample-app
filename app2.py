da1260576a19e75614c16f46af6f46df2de64757
SouhailChakk <134346382+SouhailChakk@users.noreply.github.com>
2023-12-14 15:26:18 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,25 +1,35 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, Blueprint
 from flask_wtf.csrf import CSRFProtect
 from werkzeug.utils import secure_filename
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 
+
 app2_bp = Blueprint('app2', __name__)
 csrf = CSRFProtect(app)
 
-# Configure Azure Storage
-connect_str = os.getenv('DefaultEndpointsProtocol=https;AccountName=csb10032000d733470a;AccountKey=7xF+CGBtqhNfWAoBCPQrp3cyp+qsDH+moJ9Np00KFNpkslSMNbuYW+/VzdHdxGdoZwMrDNgU5sKq+AStdVUzBQ==;EndpointSuffix=core.windows.net')
-container_name = "blobstorage"
-blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str)
 
-try:
-    container_client = blob_service_client.get_container_client(container=container_name)
-    container_client.get_container_properties()
-except Exception as e:
-    print(e)
-    print("Creating container...")
-    container_client = blob_service_client.create_container(container_name)
+def create_app():
+    app = Flask(__name__)
+    csrf = CSRFProtect(app)
+
+    # Configure Azure Storage
+    connect_str = os.getenv('DefaultEndpointsProtocol=https;AccountName=csb10032000d733470a;AccountKey=7xF+CGBtqhNfWAoBCPQrp3cyp+qsDH+moJ9Np00KFNpkslSMNbuYW+/VzdHdxGdoZwMrDNgU5sKq+AStdVUzBQ==;EndpointSuffix=core.windows.net')
+    container_name = "blobstorage"
+    blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str)
+
+    try:
+        container_client = blob_service_client.get_container_client(container=container_name)
+        container_client.get_container_properties()
+    except Exception as e:
+        print(e)
+        print("Creating container...")
+        container_client = blob_service_client.create_container(container_name)
+
+    return app
+
+app = create_app()
 
 @app2_bp.route('/upload', methods=['POST'])
 @csrf.exempt
