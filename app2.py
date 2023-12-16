63c19d1777e1cabd36acbd9feb0a73cd9be54991
SouhailChakk <134346382+SouhailChakk@users.noreply.github.com>
2023-12-16 16:56:03 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,37 +1,23 @@
-import os, uuid
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from flask import request
 
-try:
-    print("Azure Blob storage v12 - Python quickstart sample")
-    # Quick start code goes here
-except Exception as ex:
-    print('Exception:')
-    print(ex)
-
-connect_str = "DefaultEndpointsProtocol=https;AccountName=csb10032000d733470a;AccountKey=7xF+CGBtqhNfWAoBCPQrp3cyp+qsDH+moJ9Np00KFNpkslSMNbuYW+/VzdHdxGdoZwMrDNgU5sKq+AStdVUzBQ==;EndpointSuffix=core.windows.net"
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-container_name = 'csb10032000d733470a'
-container_client = blob_service_client.get_container_client(container_name)
-
-# List the blobs in the container
-print("\nListing blobs...")
-blob_list = container_client.list_blobs()
-for blob in blob_list:
-    print("\t" + blob.name)
-
-def upload_image(id, customer_image):
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
     try:
+        # Get the uploaded file from the form
+        customer_image = request.files['customer_image']
+
         # Create a container client
-        container_name = 'csb10032000d733470a'
+        container_name = 'blobstorage'
         container_client = blob_service_client.get_container_client(container_name)
 
         # Upload the image to Azure Blob Storage
-        blob_name = f'review_images/{id}_{customer_image.filename}'
+        blob_name = f'review_images/{customer_image.filename}'
         blob_client = container_client.get_blob_client(blob_name)
         blob_client.upload_blob(customer_image)
 
-        return True
+        # Save the image reference in your PostgreSQL database
+        # ...
+
+        return 'Image uploaded successfully.'
     except Exception as ex:
-        print(f'Error: {str(ex)}')
-        return False
+        return f'Error: {str(ex)}'
