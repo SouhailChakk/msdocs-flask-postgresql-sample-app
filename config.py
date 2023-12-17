5ad8b0ed328d70eb5148b73ba8376e8abd2e5ef0
SouhailChakk <134346382+SouhailChakk@users.noreply.github.com>
2023-12-17 14:09:31 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+ACCOUNT_NAME = "blolbcontainer"
+ACCOUNT_KEY = "PLwcTYeojwwpUGEaDMJ3oHQ0dS5TJKvyJpyEAD1MBiLYf8qf82CvLCvknzWxvYIsbojEJaWbV4NN+AStxNiJxw=="
+CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=blolbcontainer;AccountKey=PLwcTYeojwwpUGEaDMJ3oHQ0dS5TJKvyJpyEAD1MBiLYf8qf82CvLCvknzWxvYIsbojEJaWbV4NN+AStxNiJxw==;EndpointSuffix=core.windows.net"
+CONTAINER = "blobstorage"
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
+MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
